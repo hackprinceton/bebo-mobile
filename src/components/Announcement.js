9f7fdee661f239e7a878936d1c3a3746f747b644
PaliC <sahancpal@gmail.com>
2017-10-16 16:34:04 -0400

    fixed errors

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -7,20 +7,9 @@ import {
 import FontAwesome, { Icons } from 'react-native-fontawesome'
 import { styles } from '../styles'
 
-// const DATE_FORMAT = 'ddd h:mm a';
-// const TIME_ZONE = 'America/New_York';
-//
-// const formatTime = time => {
-//   return moment(time).tz(TIME_ZONE).format(DATE_FORMAT);
-// }
 
 export const Announcement = anoun => {
   console.log("rendering announcement " + JSON.stringify(anoun))
-  //const timeStr = formatTime(anoun.item.start) + '-' + formatTime(anoun.item.end);
-
-  // const location = (anoun.item.location) ?
-  //   <Text style={styles.body}><FontAwesome>{Icons.mapMarker}</FontAwesome> {anoun.item.location}</Text> :
-  //   null
   return (
     <View style={styles.dayofEvent}>
       <Text style={styles.title}>{anoun.item.title}</Text>
--- src/reducers/announcements.js
+++ src/reducers/announcements.js
@@ -1,4 +1,4 @@
-export default events = (state = [], action) => {
+export default announcements = (state = [], action) => {
   switch (action.type) {
     case 'LOAD_ANNOUNC_SUCC':
       return action.announcements
